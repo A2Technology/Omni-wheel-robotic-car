8a58746476bf34bedcab0ef12b73a7d2241e0980
Tran Tieu Binh <tieubinhco@gmail.com>
2020-09-08 23:57:56 +0700

    Write functions to file .cpp

---
+++ Functions in file/Motor_control.cpp
@@ -0,0 +1,44 @@
+void w1(int rotation, int direct)
+{
+    analogWrite(PWM1, rotation);
+    if (direct == 1)
+    {
+        digitalWrite(IN1A, HIGH);
+        digitalWrite(IN2A, LOW);
+    }
+    else if (direct == -1)
+    {
+        digitalWrite(IN1A, LOW);
+        digitalWrite(IN2A, HIGH);
+    }
+}
+
+void w2(int rotation, int direct)
+{
+    analogWrite(PWM2, rotation);
+    if (direct == 1)
+    {
+        digitalWrite(IN1B, HIGH);
+        digitalWrite(IN2B, LOW);
+    }
+    else if (direct == -1)
+    {
+        digitalWrite(IN1B, LOW);
+        digitalWrite(IN2B, HIGH);
+    }
+}
+
+void w3(int rotation, int direct)
+{
+    analogWrite(PWM3, rotation);
+    if (direct == 1)
+    {
+        digitalWrite(IN1C, HIGH);
+        digitalWrite(IN2C, LOW);
+    }
+    else if (direct == -1)
+    {
+        digitalWrite(IN1C, LOW);
+        digitalWrite(IN2C, HIGH);
+    }
+}
